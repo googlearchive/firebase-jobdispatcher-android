717fabad9d2d238adc72fe09853dbc4603cbba83
Alex Saveau <SUPERCILEX@users.noreply.github.com>
2016-11-05 20:35:23 -0700

    Update Constraint.java

--- jobdispatcher/src/main/java/com/firebase/jobdispatcher/Constraint.java
+++ jobdispatcher/src/main/java/com/firebase/jobdispatcher/Constraint.java
@@ -32,7 +32,7 @@ public final class Constraint {
     public static final int ON_UNMETERED_NETWORK = 1;
 
     /**
-     * Only run the job when a network connection is available. If both this and ON_ANY_NETWORK is
+     * Only run the job when a network connection is available. If both this and ON_UNMETERED_NETWORK is
      * provided, ON_ANY_NETWORK will take precedence.
      */
     public static final int ON_ANY_NETWORK = 1 << 1;
